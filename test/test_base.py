ce2a560463672edcb41afd4c8c8e4cc9036e0ce2
Vincent Laurent <vincent.laurent@mews-labs.com>
2025-06-16 09:49:25 +0200

    [CI] add pdf

--- test/test_base.py
+++ test/test_base.py
@@ -12,7 +12,7 @@ from papyrus.engine import (
     CamelotExtractor,
 )
 
-path = "./papyrus/data_pdf/invoice_100.pdf"
+path = "./papyrus/test/invoice_100.pdf"
 
 @pytest.mark.parametrize(
     "extractor, extractor_name, method_name, expected_capability",
